bae65dcc3755e3a30c7cc50b1e0933014468f581
felix_vondee <felixvondee8@gmail.com>
2021-03-30 11:09:06 +0200

    Creates test for my_each on hash

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,8 +2,13 @@ require_relative '../enumerables'
 describe Enumerable do
     describe "#my_each" do
       
-      it "prints the elemsnt in the given range" do
+      it "Returns enumerable when no block given" do
         expect((1..5).my_each).to be_a(Enumerable)
+      end
+      
+      it "Returns itself when block given" do
+        expect((1..5).my_each {|e| e}).to eq((1..5))
+        expect({key1:10, key2:'Felix'}.my_each {|e| e}).to eq({key1:10, key2:'Felix'})
       end
     end
 end
